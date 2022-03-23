a1ddbc8a7f5849575a7faa8ab268feac3b40ddae
mariana_gt05507 <mfreitas.sm@gmail.com>
2022-03-23 18:11:43 +1100

    vUe form and acessbility

---
+++ vue-basic-form/src/components/BaseRadio.vue
@@ -0,0 +1,39 @@
+<!--<template>
+  <input
+      type="radio"
+      v-model="event.pets"
+      :value="1"
+      name="pets"
+    />
+  <label>Yes</label>
+</template>-->
+<template>
+  <input type="radio" :checked="modelValue === value" :value="value" @change="$emit('update:modelValue', value)" v-bind="$attrs" :id="uuid" />
+  <label :for="uuid" v-if="label">{{ label }}</label>
+</template>
+
+<script>
+import UniqueID from "../features/UniqueID";
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    value: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+  setup() {
+    const uuid = UniqueID().getID();
+    return {
+      uuid,
+    };
+  },
+};
+</script>
