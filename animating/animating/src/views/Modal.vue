82ff4f93bfa22bfd39121963c04c5ed140ad0cdb
mariana_gt05507 <mfreitas.sm@gmail.com>
2022-03-31 17:06:57 +1100

    Group transitions

--- animating/animating/src/views/Modal.vue
+++ animating/animating/src/views/Modal.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <h1>This is an modal page</h1>
-    >
     <button @click="toggleModal">Open</button>
     <transition name="fade">
       <div v-if="isOpen" class="modal">
